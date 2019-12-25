23877f1226cb609ae96addd8b18b918adac4760e
Alikhan Rassilov <arassilov@gmail.com>
2019-12-25 16:39:17 +0600

    Exercise_02

--- Lesson_01/exercise_01.py
+++
@@ -1,8 +0,0 @@
-name = input("Введите имя: ")
-age = int(input("Введите возраст: "))
-country = input("Введите страну: ")
-
-if age % 5 == 0:
-    print(f"Меня зовут {name}, мне {age} лет, я из {country}")
-elif age % 5 > 0 and (age // 5) % 2 == 0:
-    print(f"Меня зовут {name}, мне {age} год(а), я из {country}")
--- Lesson_01/exercise_03.py
+++
@@ -1,9 +0,0 @@
-number = input("Введите любое число: ")
-
-number_1 = int(number)
-number_2 = int(number * 2)
-number_3 = int(number * 3)
-
-sum = number_1 + number_2 + number_3
-
-print(sum)
