24f858ae71aec34928d0563178f58390a4da3b13
Alikhan Rassilov <arassilov@gmail.com>
2019-12-25 16:34:17 +0600

    Exercise_01

--- exercise_01.py
+++ exercise_01.py
@@ -5,5 +5,4 @@ country = input("Введите страну: ")
 if age % 5 == 0:
     print(f"Меня зовут {name}, мне {age} лет, я из {country}")
 elif age % 5 > 0 and (age // 5) % 2 == 0:
-    print(f"Меня зовут {name}, мне {age} год(а), я из {country}")
-
+    print(f"Меня зовут {name}, мне {age} год(а), я из {country} ")
