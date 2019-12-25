8955ea17b38f3e1b93126663b8ea43747c72e8a5
Alikhan Rassilov <arassilov@gmail.com>
2019-12-25 16:43:53 +0600

    Exercise_03

--- Lesson_01/exercise_02.py
+++
@@ -1,7 +0,0 @@
-seconds_from_user = int(input("Введите время в секундах: "))
-
-hours = (seconds_from_user // 60) // 60
-minutes = (seconds_from_user // 60) % 60
-seconds = seconds_from_user % 60
-
-print(f"{hours}:{minutes}:{seconds}")
